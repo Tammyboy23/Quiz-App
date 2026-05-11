819d2b04785b62a963d2cb09beb89d9fb4d0ae30
Tammyboy23 <fagboyeguntamilore43@gmail.com>
2026-05-11 02:11:04 +0100

    Updated Quiz Functionality

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,5 +1,5 @@
-function Page(){
-    const quiz = [
+import { useState } from "react";
+const quiz = [
     {question: "What is the capital of nigeria",
     answers: [
         "Anambra",
@@ -19,11 +19,37 @@ function Page(){
     correct: "DC"
 }
     ]
+function Page(){
+    const [currentQuestion, setcurrentQuestion] = useState(0);
+    const [score, setscore] = useState(0);
+    const currentQuiz = quiz[currentQuestion]
+    
+    function handleAns(ans){
+        if(ans === currentQuiz.correct){
+            setscore(score + 1)
+        }
+        setcurrentQuestion(currentQuestion + 1);
+    }
+   
     return(
         <>
         <div className="app">
             <h1>hello quiz app</h1>
-            
+            {currentQuestion < quiz.length ? (
+                <>
+                    <h2>{currentQuiz.question}</h2>
+                    <div className="buttons">
+                        {currentQuiz.answers.map((ans) => (
+                            <button key={ans} onClick={() => handleAns(ans)}>
+                                {ans}
+                            </button>
+                        ))}
+                    </div>
+                </>
+            ) : (
+                <h2>Quiz Complete! Final Score: {score}/{quiz.length}</h2>
+            )}
+            <h4>Score: {score}</h4>
         </div>
         </>
     )
