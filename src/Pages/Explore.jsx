94add068331cce33e1971c2bf61f9563a5c3adaa
Fagboyegun Oluwatamilore <167362641+Tammyboy23@users.noreply.github.com>
2026-06-06 12:09:45 +0100

    Update Explore.jsx
    updated Claude explore page

--- src/Pages/Explore.jsx
+++ src/Pages/Explore.jsx
@@ -8,19 +8,43 @@ function Explore() {
   return (
     <>
       <div className="explore">
-        <div className="quizes">
-          {quizes.map((quiz) => (
-            <div className="quizy" key={quiz.quiz}>
-              <img src={quiz.img} alt="" />
-              <h1>{quiz.name}</h1>
-              <p>{quiz.desc}</p>
-              <span><LuFileStack /> {quiz.length} Qs</span>
-              <Link to={`/quiz/${quiz.quiz}`}>
-                <button>Start Quiz <LiaArrowRightSolid /></button>
+      <div className="explore-header">
+        <div className="explore-eyebrow">All Quizzes</div>
+        <h1 className="explore-title">What do you want to test today?</h1>
+        <p className="explore-subtitle">
+          Pick a category and put your knowledge to the test.
+        </p>
+      </div>
+
+      <div className="quizes">
+        {quizes.map((quiz, i) => (
+          <div
+            className="quizy"
+            key={quiz.quiz}
+            style={{ "--delay": `${i * 60}ms` }}
+          >
+            <div className="quizy-inner">
+              <div className="quizy-top">
+                <div className="quizy-img-wrap">
+                  <img src={quiz.img} alt={quiz.name} />
+                </div>
+                <span className="quizy-count">
+                  <LuFileStack />
+                  {quiz.length} Qs
+                </span>
+              </div>
+
+              <div className="quizy-body">
+                <h2 className="quizy-name">{quiz.name}</h2>
+                <p className="quizy-desc">{quiz.desc}</p>
+              </div>
+
+              <Link to={`/quiz/${quiz.quiz}`} className="quizy-link">
+                Start Quiz <LiaArrowRightSolid className="quizy-arrow" />
               </Link>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </>
   );
