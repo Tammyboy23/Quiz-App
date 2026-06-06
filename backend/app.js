4467aca1f1b26933f574a6a2333f33223517f946
Tammyboy23 <fagboyeguntamilore43@gmail.com>
2026-06-06 13:22:05 +0100

    Styled create page

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Quizora Server Running")
 })
