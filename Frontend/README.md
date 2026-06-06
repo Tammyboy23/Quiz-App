c2ad6ba0fad0926f076d26a50ff0ac0fc24c3ce6
Tammyboy23 <fagboyeguntamilore43@gmail.com>
2026-06-06 13:30:46 +0100

    New Create pdf page

--- Frontend/README.md
+++ Frontend/README.md
@@ -1,6 +1,6 @@
-# QUIZORA
+# QUIZORA ❓
 This is Quizora a quiz app made for `Babcock` University Mid-semester as a mini project you can check it in [Quizora](https://quizora-tammy.netlify.app)
-## Features
+## 🗒️ Features
 It has many Features which i would be listing for you below
 ### 1. No of Questions
 As the name implies i can select the number of questions from the total number of questions using an input range and using `quiz.lenght` to identify the total number of questions given.
--- Frontend/src/Pages/Create.jsx
+++ Frontend/src/Pages/Create.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FaLaptop } from "react-icons/fa";
+import { LuArrowLeft } from "react-icons/lu";
 
 function Create(){
     const [mode, setmode] = useState("");
@@ -21,7 +22,7 @@ function Create(){
                 </div>
         ) : (
             <div className="second">
-                <button onClick={() => setmode("")} className="back">Back</button>
+                <button onClick={() => setmode("")} className="back"> < LuArrowLeft />Back</button>
                 {mode === "Text" ? (
                     <div className="written">
                     <h3>Written Mode</h3>
@@ -30,7 +31,24 @@ function Create(){
                     </div>
                 ): (
                     <div className="pd">
-
+                        <h3>PDF MODE</h3>
+                        
+<form class="file-upload-form">
+  <label class="file-upload-label" for="file">
+    <div class="file-upload-design">
+      <svg height="1em" viewBox="0 0 640 512">
+        <path
+          d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z"
+        ></path>
+      </svg>
+      <p>Drag and Drop</p>
+      <p>or</p>
+      <span class="browse-button">Browse file</span>
+    </div>
+    <input type="file" id="file" />
+  </label>
+</form>
+<button>Create <FaLaptop /></button>
                     </div>
                 )}
             </div>
